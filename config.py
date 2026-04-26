5fca453ac006ccb2d944a36dc86e85edcbc462d0
raemarie999-jpg <raemarie999@gmail.com>
2026-04-26 15:08:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_KEY = os.getenv("MINUTETEMP_API_KEY")
 
-CITIES = ["chi", "dal", "nyc"]
+CITIES = ["KMDW", "KDFW", "NYC"]
 
 if not API_KEY:
     print("⚠️ WARNING: MINUTETEMP_API_KEY is missing")
